6c703cdf377a584030384fbc9400f21fea4f7525
xkond <xkondimension@163.com>
2024-08-07 19:01:29 +0800

    Remove .idea folder

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# zufangCrawler
+
+# zufangCrawler 仓库描述
+
 Rental Information Crawler
+
+租房信息爬虫，抓取**58同城，安居客，链家，贝壳**等租房信息
+
+原型项目：**https://github.com/NanmiCoder/MediaCrawler**
+
---
+++ main.py
@@ -0,0 +1,18 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 Double Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+import asyncio
+import sys
+
+
+
+async def main():
+    return
+
+if __name__ == '__main__':
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        sys.exit()
